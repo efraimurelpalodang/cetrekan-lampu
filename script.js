230c5f24b75869ac73815c6caa929c7330d1291f
efraim urel <efraimurelpalodang@gmail.com>
2025-04-03 15:44:09 +0800

    membuat toggle tapi belum selesai

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-function saklar(act, lamp) {
-  const lampu1 = document.querySelector('#lampu1');
-  const lampu2 = document.querySelector('#lampu2');
-  const lampu3 = document.querySelector('#lampu3');
+function saklar(lamp) {
 
-  if( act == 'on' ) {
-    if( lamp == 1 ) {
-      lampu1.src = 'assets/images/on.gif';
-    } else if ( lamp == 2 ) {
-      lampu2.src = 'assets/images/on.gif';
-    } else if ( lamp == 3 ) {
-      lampu3.src = 'assets/images/on.gif';
+  const tombol = document.getElementById("tombol-tgl");
+    
+  const lampu1 = document.querySelector("#lampu1");
+  const lampu2 = document.querySelector("#lampu2");
+  const lampu3 = document.querySelector("#lampu3");
+  
+  if( tombol.checked ) {
+    if (lamp == 1) {
+      lampu1.src = "assets/images/on.gif";
+    } else if (lamp == 2) {
+      lampu2.src = "assets/images/on.gif";
+    } else if (lamp == 3) {
+      lampu3.src = "assets/images/on.gif";
     }
-  } else if ( act == 'off' ) {
+  } else {
     if (lamp == 1) {
       lampu1.src = "assets/images/off.gif";
     } else if (lamp == 2) {
@@ -21,6 +24,4 @@ function saklar(act, lamp) {
     }
   }
 
-  return lampu1;
-  
 }

3f8b4ada7145b5d265f9d6c916e35ad1a7bc95e1
efraim urel <efraimurelpalodang@gmail.com>
2025-04-07 09:45:17 +0800

    selesai membuat setrekan lampu

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 function saklar(lamp) {
 
   const tombol = document.getElementById("tombol-tgl");
+  const tombol1 = document.getElementById("tombol-tgl2");
+  const tombol2 = document.getElementById("tombol-tgl3");
     
   const lampu1 = document.querySelector("#lampu1");
   const lampu2 = document.querySelector("#lampu2");
   const lampu3 = document.querySelector("#lampu3");
-  
-  if( tombol.checked ) {
-    if (lamp == 1) {
-      lampu1.src = "assets/images/on.gif";
-    } else if (lamp == 2) {
-      lampu2.src = "assets/images/on.gif";
-    } else if (lamp == 3) {
-      lampu3.src = "assets/images/on.gif";
-    }
+
+  if (tombol.checked) {
+    lampu1.src = 'assets/images/on.gif'    
   } else {
-    if (lamp == 1) {
-      lampu1.src = "assets/images/off.gif";
-    } else if (lamp == 2) {
-      lampu2.src = "assets/images/off.gif";
-    } else if (lamp == 3) {
-      lampu3.src = "assets/images/off.gif";
-    }
+    lampu1.src = "assets/images/off.gif";
+  }
+  if (tombol1.checked) {
+    lampu2.src = 'assets/images/on.gif'    
+  } else {
+    lampu2.src = "assets/images/off.gif";
+  }
+  if (tombol2.checked) {
+    lampu3.src = 'assets/images/on.gif'    
+  } else {
+    lampu3.src = "assets/images/off.gif";
   }
 
 }
